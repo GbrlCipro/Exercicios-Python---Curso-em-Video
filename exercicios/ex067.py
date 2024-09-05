49b81f1ee98fd708496772e659f25b57f960ac71
Gabriel Cipriano <121181206+GbrlCipro@users.noreply.github.com>
2024-09-05 17:58:19 -0300

    Comits 05-09

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,1 +1,27 @@
-# Faça um programa que mostra a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+'''def tabuada(n):
+    multiplicador = n
+    multiplicando = 1
+    resultado = ''
+    while multiplicando <= 10:
+        resultado += f'{multiplicador} X {multiplicando} = {multiplicador * multiplicando}\n'
+        multiplicando += 1
+    #print(resultado)
+    return resultado
+
+print(tabuada(1))'''
+
+n = 0
+while True:
+    n = int(input('Digite o multiplicador: '))
+    if n >= 0:
+        multiplicador = n
+        multiplicando = 1
+        resultado = ''
+        while multiplicando <= 10:
+            resultado += f'{multiplicador} X {multiplicando} = {multiplicador * multiplicando}\n'
+            multiplicando += 1
+        print(resultado)
+    else:
+        break
--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -1,1 +1,41 @@
 # Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
+import random
+
+vencedor = ''
+
+palpJog = 'Par'
+numJog = 1
+
+palpMaq = random.randint(0, 1)
+if palpMaq == 1:
+    palpMaq = 'Impar'
+else:
+    palpMaq = 'Par'
+
+numMaq = random.randint(1, 5)
+
+resultNum = numJog + numMaq
+
+print('Escolha da maquina: ', numMaq)
+print('Escolha do jogador: ', numJog)
+
+if resultNum % 2 == 0:
+    resultNum = 'É PAR'
+else:
+    resultNum = 'É ÍMPAR'
+
+print('Resultado: ', resultNum)
+
+if palpJog == 'Impar' and resultNum == 'É ÍMPAR':
+    vencedor = 'Jogador'
+    print('O vencedor foi ', vencedor)
+if palpJog == 'Par' and resultNum == 'É PAR':
+    vencedor = 'Jogador'
+    print('O vencedor foi ', vencedor)
+elif palpMaq == 'Impar' and resultNum == 'É ÍMPAR':
+    vencedor = 'Maquina'
+    print('O vencedor foi ', vencedor)
+elif palpMaq == 'Par' and resultNum == 'É PAR':
+    vencedor = 'Maquina'
+    print('O vencedor foi ', vencedor)
