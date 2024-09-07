92421d2fd3d46c7fe4833606b5232a09a8bbeb79
Gabriel Cipriano <121181206+GbrlCipro@users.noreply.github.com>
2024-09-07 19:52:59 -0300

    Changes to be committed: 	modified:   exercicios/ex069.py 	modified:   exercicios/ex070.py

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -4,3 +4,38 @@
 # B) Quantos homens foram cadastrados;
 # C) Quantas mulheres tem menos de 20 anos.
 
+import time
+
+mais18 = 0
+homem = 0
+mulher20 = 0
+
+
+while True:             # inicializando o loop que vai funcionar até que o usuário responda "Sim"    
+
+    idade = int(input("Idade: "))
+
+    sexo = str(input('Sexo: [M/F]')).strip().upper()[0]  # Bloquinho pra capturar o sexo, e garantir que o usuario insira corretamente 
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+
+    resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+                        # Verficando quantas pessoas tem mais de 18 anos, e fazendo a contagem delas
+    if idade > 18:
+        mais18 += 1
+                        # Verificando quantos homens foram listados, e fazendo a contagem deles
+    if sexo == "M":
+        homem += 1
+                        # Verficando quantas mulheres tem menos de 20 anos, e fazendo a contagem delas
+    if sexo == "F" and idade < 20:
+        mulher20 +=1
+                        # Verificando a resposta da pergunta: "Quer continuar?". Caso seja "N" de não, o programa breaka
+    if resp == 'N':
+        time.sleep(1)
+        print('\nVocê escolheu sair, o programa vai ser encerrado.\n')
+        time.sleep(1)
+        print(f'Pesssoas com mais de 18 anos: {mais18}\nQuantidade de homens: {homem}\nQuantidade de mulheres com menos de 20 anos: {mulher20}\n')
+        time.sleep(1)        
+        break
--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -3,3 +3,35 @@
 # A) Qual é o total gasto na compra;
 # B) Quantos produtos custam mais de R$ 1000;
 # C) Qual é o nome do produto mais barato.
+
+import time
+
+totalCompra = 0
+prod1000 = 0
+menorPreco = float('inf')
+maisBarato = ''
+
+while True:             # inicializando o loop que vai funcionar até que o usuário responda "Sim"
+
+    produto = str(input('Produto: '))
+    preco = float(input('Preço: '))
+
+    if preco >= 1000:
+        prod1000 += 1
+    
+    if preco < menorPreco:
+        menorPreco = preco
+        maisBarato = produto
+
+    totalCompra += preco
+
+                        # Verificando a resposta da pergunta: "Quer continuar?". Caso seja "N" de não, o programa breaka
+    resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        print(f'\nTotal gasto na compra: {totalCompra}\nQuantos produtos custaram mais de 1000,00 reais? {prod1000}\nO produto mais barato foi: {maisBarato}\n')
+        time.sleep(1)
+        print('\nVocê escolheu sair, o programa vai ser encerrado.\n')
+        time.sleep(1)
+        break
