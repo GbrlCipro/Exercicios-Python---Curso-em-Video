1fcaa95a64e8151a2aacc380ad48026f7c097397
Gabriel Cipriano <121181206+GbrlCipro@users.noreply.github.com>
2024-09-15 21:47:58 -0300

    Changes to be committed: 	new file:   exercicios/ex072.py 	new file:   exercicios/ex073.py 	new file:   exercicios/ex074.py 	new file:   exercicios/ex075.py 	new file:   exercicios/ex076.py 	new file:   exercicios/ex077.py

---
+++ exercicios/ex073.py
@@ -0,0 +1,5 @@
+# Crie uma tupla com os 20 primeiros colocados da tabela do campeonato brasileiro de futebol, na ordem de colocação. Depois mostre: 
+# A) Apenas os 5 primeiros colocados;
+# B) Os 4 últimos colocados;
+# C) Uma lista com os times em ordem alfabética;
+# D) Em que posição da tabela está o time Chapecoense.
---
+++ exercicios/ex077.py
@@ -0,0 +1,1 @@
+# Crie um programa que tenha uma tupla com várias palavras. Depois disso você deve mostrar para cada palavra, quais foram suas vogais
