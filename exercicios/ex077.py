e4763256671d930010f7c5d0d3beae006deef741
Gabriel Cipriano <121181206+GbrlCipro@users.noreply.github.com>
2024-09-20 16:56:05 -0300

    Update ex077.py

--- exercicios/ex077.py
+++ exercicios/ex077.py
@@ -1,21 +1,24 @@
 # Crie um programa que tenha uma tupla com várias palavras. Depois disso você deve mostrar para cada palavra, quais foram suas vogais
 
 nomes = []
+tp=tuple(nomes)
 
 def gravarPlvr():
     n = entr.get()
     nomes.append(n)
     entr.delete(0, 'end')
 
-def vogaisPorPalavra():
-    v = []
+def getVogais():
+    result = ''
     for n in nomes:
+        v=[]
         for ltr in n:
             if ltr in 'aeiou':
                 v.append(ltr)
-    res.config(text=f'{n}, {v}') # ESTA TRAZENDO SO O ULTIMO NOME DIGITADO 
+        result += f'{n}: {" ".join(v)}\n{tp}\n'
+    res.config(text=result)
 
-def teste():
+def test():
     res.config(text=nomes)
 
 def close():
@@ -23,18 +26,20 @@ def close():
 
 import tkinter as tk
 root = tk.Tk()
-txt1 = tk.Label(root, text='texto 1')
+txt1 = tk.Label(root, text='Digite um nome para ver suas vogais')
 txt1.pack()
 entr = tk.Entry(root)
 entr.pack()
-btn = tk.Button(root, text='Gravar', command=gravarPlvr)
+btn = tk.Button(root, text='Gravar nome', command=gravarPlvr)
 btn.pack()
-btn1 = tk.Button(root, text='clique', command=vogaisPorPalavra)
-btn1.pack()
-btn2 = tk.Button(root, text='FECHAR', command=close)
-btn2.pack()
-btn3 = tk.Button(root, text='imprimir', command=teste)
+btn3 = tk.Button(root, text='Mostrar nome', command=test)
 btn3.pack()
-res = tk.Label(root, text='resultado apos o clique')
+btn1 = tk.Button(root, text='Resultado', command=getVogais)
+btn1.pack()
+btn2 = tk.Button(root, text='Fechar', command=close)
+btn2.pack()
+res = tk.Label(root, text='')
 res.pack()
+#root.bind('<Return>', gravarPlvr, close, test, getVogais)
+
 root.mainloop()
